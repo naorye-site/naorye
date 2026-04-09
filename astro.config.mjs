08a759b804b1e536d5b8e9edff53de35722b51b1
gloriazaodyardy <gloriazalawrance@gmail.com>
2026-04-09 23:29:30 +0700

    improve footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import sanity from "@sanity/astro";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
@@ -12,7 +14,12 @@ export default defineConfig({
       projectId: "q9our54l",
       dataset: "production",
       useCdn: false,
-      apiVersion: '2026-04-09',
+      apiVersion: "2026-04-09",
+    }),
+    icon({
+      include: {
+        cib: ["ko-fi", "pinterest"]
+      },
     }),
   ],
   vite: {
